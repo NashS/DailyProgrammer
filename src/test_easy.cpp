67759d58f91499ed55322439159d48d4cc048e6e
NashS <nasseq@gmail.com>
2016-09-04 08:42:23 -0400

    Revert "Revert "Revert "Revert "Revert "Revert "ayy""""""
    This reverts commit 643f1bd12836c73ab8792f46230bc91158d34339.

---
+++ src/test_easy.cpp
@@ -0,0 +1,15 @@
+/*
+ * test_easy.c
+ *
+ *  Created on: Aug 19, 2016
+ *      Author: beefcake
+ */
+#include <stdio.h>
+#include <string.h>
+
+int
+main (int argc, char * argv[])
+{
+    printf("Hello World!");
+    return 0;
+}
